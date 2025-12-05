cda7dab08958d298a4318cee688fac8b262342ac
xmannii <manimirzaei674@gmail.com>
2025-12-05 20:04:04 +0330

    fix: remove dotenv import - Next.js handles env vars automatically

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,9 +1,6 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./schema";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const connectionString = process.env.DATABASE_URL!;
 
